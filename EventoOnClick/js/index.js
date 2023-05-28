b5c759446a7bbe7e247e940ae5796c44c66ab6d0
GIO-MELLA <133301923+GIO-MELLA@users.noreply.github.com>
2023-05-27 23:02:13 -0400

    Tarea EventoOnClick

---
+++ EventoOnClick/js/index.js
@@ -0,0 +1,17 @@
+
+function mensaje(nombre)
+{
+    alert(nombre + " was liked :)");
+}
+
+function hideButton(elemento)
+{
+    elemento.remove();
+}
+
+function changeButton(elemento)
+{
+    document.getElementById('Login').innerHTML = 'Logout';
+}
+
+//Giovanna Mella
